92f52f65afb9d716e5fbf701f92012a9fa830652
Vega Nicholas S. <veganicholas123@gmail.com>
2024-06-29 13:30:47 +0700

    chore: initialize project 🚀

---
+++ PDFMerger.php
@@ -0,0 +1,70 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use setasign\Fpdi\Fpdi;
+
+const FOLDER_PATH = __DIR__ . '/files_to_merge/';
+const OUTPUT_PATH = __DIR__ . '/merged_files/';
+
+const PDF_FORMAT = '.pdf';
+
+function merge_pdfs($pdf_files, $output_file) {
+    $pdf = new FPDI();
+
+    foreach($pdf_files as $file){
+        $pageCount = $pdf->setSourceFile($file);
+        for($pageNo = 1; $pageNo <= $pageCount; $pageNo++){
+            $templateId = $pdf->importPage($pageNo);
+            $size = $pdf->getTemplateSize($templateId);
+
+            $pdf->AddPage($size['orientation'], [$size['width'], $size['height']]);
+            $pdf->useTemplate($templateId);
+        }
+    }
+
+    $pdf->Output('F', OUTPUT_PATH . $output_file);
+}
+
+$all_pdf_files = glob(FOLDER_PATH . '*' . PDF_FORMAT);
+
+if(count($argv) < 2){
+    echo "Please enter the output file name.";
+    return;
+}
+
+if(!$all_pdf_files){
+    echo "Failed to load pdf files.";
+    return;
+}elseif(count($all_pdf_files) === 0){
+    echo "Please enter files, at least 2 files or more.";
+    return;
+}
+
+$pdf_files = [];
+
+foreach($all_pdf_files as $pdf_file){
+    $file_page = (int) preg_replace('/[^0-9]/', '', $pdf_file);
+    $pdf_files[$file_page] = $pdf_file;
+}
+
+ksort($pdf_files);
+
+$output_files = glob(OUTPUT_PATH . '*' . PDF_FORMAT);
+
+$output_name = (string) $argv[1];
+if(!str_contains($output_name, PDF_FORMAT)){
+    $output_name .= PDF_FORMAT;
+}
+
+if(file_exists(OUTPUT_PATH . $output_name)){
+    $file_info = pathinfo($output_name);
+    $output_name = str_replace(" ", "_", $file_info['filename'] . " (" . count($output_files) . ")" . PDF_FORMAT);
+}
+
+$start = microtime(true);
+
+merge_pdfs($pdf_files, $output_name);
+
+echo "Done in " . round(microtime(true) - $start, 3) . "s" . PHP_EOL;
+echo "Successfully merged PDFs into $output_name";
---
+++ README.md
@@ -0,0 +1,17 @@
+# PDFMerger Files
+
+## Overview
+
+This script merges multiple PDF files into a single PDF file. It uses the FPDI library to handle the PDF merging process.
+
+## Usage
+
+### Command Line Arguments
+
+1. **Output File Name**: Provide the desired output file name as a command line argument.
+
+### Example Usage
+
+```bash
+php PDFMerger.php file_name.pdf
+```
